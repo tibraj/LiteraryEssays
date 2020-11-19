9cebda11174a4ead5f957eb263749259ab3ea121
tibraj <tibraj94@gmail.com>
2020-11-19 15:05:47 -0500

    onChange event handler

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -4,6 +4,14 @@ import { updateSignup } from '../actions/signup.js'
 import { signup } from '../actions/currentUser.js';
 
 const Signup = ({}) => {
+    const handleOnChange = event => {
+        const {name, value} = event.target
+        const updatedInfo = {
+            ...signupData,
+            [name]: value 
+        }
+        updateSignup(updatedInfo)
+    }
     return (
         <form></form>
     )
