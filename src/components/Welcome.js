26b03c41369e35a047bcf11b5a2bd689803ec736
tibraj <tibraj94@gmail.com>
2020-11-19 15:15:01 -0500

    welcome page component with link to signup

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
+const Welcome = () => {
+    return (
+        <div>
+            <h1>Welcome To React/Redux Essays</h1>
+            <Link to="/signup">Signup</Link>
+        </div>
+    )
+}
