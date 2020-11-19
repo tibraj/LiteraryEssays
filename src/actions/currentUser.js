e7c3a269b059b08ce508920ded9bf28a68458aed
tibraj <tibraj94@gmail.com>
2020-11-19 14:59:32 -0500

    exporting signup action (post fetch request, setting current user)

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -5,6 +5,30 @@ export const setCurrentUser = user => {
     }
 }
 
+export const signup = (credentials, history) => {
+    return dispatch => {
+        const information = {
+            user: credentials
+        }
+        return fetch("http://localhost:3000/api/v1/signup", {
+            credentials: "include",
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+        })
+            .then(response => response.json())
+            .then(json => {
+                if (json.error) {
+                    alert(json.error)
+                } else {
+                    dispatch(setCurrentUser(json))
+                    dispatch(clearSignup())
+                    history.push('/')
+                }
+            })
+            .catch(console.log)
+    }
+}
+
 export const clearCurrentUser = () => {
     return {
         type: "CLEAR_CURRENT_USER"
