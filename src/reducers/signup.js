d281cedd933397a3ac827c9978d5d5102d8995b0
tibraj <tibraj94@gmail.com>
2020-11-19 14:53:33 -0500

    switch case to update signup form

--- src/reducers/signup.js
+++ src/reducers/signup.js
@@ -3,3 +3,12 @@ const initialState = {
     username: "",
     password: ""
 }
+
+export default (state=initialState, action) => {
+    switch (action.type) {
+        case "UPDATE_SIGNUP":
+            return action.data
+        default:
+            return state
+    }
+}
