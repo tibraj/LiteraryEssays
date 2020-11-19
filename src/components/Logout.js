957fb81b55d11b422577ec61a41d0bf490fd9fe9
tibraj <tibraj94@gmail.com>
2020-11-19 17:37:19 -0500

    logout component boilerplate

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -2,3 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { logout } from '../actions/currentUser.js';
+
+const Logout = ({}) => {
+    return (
+        <form>
+
+        </form>
+    )
+}
+
+export default Logout
