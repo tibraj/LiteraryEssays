89d54867c5e65cd7bd55faa1c2379c02a9065dc4
tibraj <tibraj94@gmail.com>
2020-11-19 14:53:59 -0500

    switch case to clear signup form

--- src/reducers/signup.js
+++ src/reducers/signup.js
@@ -8,6 +8,8 @@ export default (state=initialState, action) => {
     switch (action.type) {
         case "UPDATE_SIGNUP":
             return action.data
+        case "CLEAR_SIGNUP":
+            return initialState
         default:
             return state
     }
