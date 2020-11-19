0c09da2362dc8f1c0ae2054d9780323e09fe60d9
tibraj <tibraj94@gmail.com>
2020-11-19 15:17:55 -0500

    import Provider

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './store.js';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
