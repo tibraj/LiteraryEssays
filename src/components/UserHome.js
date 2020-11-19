a419aca9f00c841df42cfb0cfa2e25d156759d52
tibraj <tibraj94@gmail.com>
2020-11-19 17:10:58 -0500

    user homepage boilerplate

--- src/components/UserHome.js
+++ src/components/UserHome.js
@@ -1,3 +1,13 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
+
+const UserHome = ({}) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default UserHome
