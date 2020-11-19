5346b34b41920f13774d9b84e9d6d0dd5ace848f
tibraj <tibraj94@gmail.com>
2020-11-19 15:27:41 -0500

    switch case to clear login form

--- src/reducers/login.js
+++ src/reducers/login.js
@@ -7,6 +7,8 @@ export default (state=initialState, action) => {
     switch (action.type) {
         case "UPDATE_LOGIN":
             return action.data
+        case "CLEAR_LOGIN":
+            return initialState
         default:
             return state
     }
