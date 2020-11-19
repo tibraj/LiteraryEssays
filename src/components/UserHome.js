38d3059e67b8fd3ca725b30cc920931c79029313
tibraj <tibraj94@gmail.com>
2020-11-19 17:42:32 -0500

    added logout component to user's homepage

--- src/components/UserHome.js
+++ src/components/UserHome.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
+import Logout from './Logout;';
 
 const UserHome = ({currentUser, loggedIn}) => {
     return (
         <div>
             {loggedIn ? <h1>{currentUser.name}'s Homepage</h1> : null}
+            <Logout />
         </div>
     )
 }
