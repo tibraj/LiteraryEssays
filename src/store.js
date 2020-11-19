6ff825a461ac15ced1afd6a957814fdbf6426c1d
tibraj <tibraj94@gmail.com>
2020-11-19 15:43:56 -0500

    import and add login reducer

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import signup from './reducers/signup.js';
+import login from './reducers/login.js';
 import currentUser from './reducers/currentUser.js';
 
 const reducer = combineReducers({
     signup,
+    login,
     currentUser
 })
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
