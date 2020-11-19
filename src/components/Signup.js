96ca66c3f95bbb6c81cf99f663bba16e61ec9329
tibraj <tibraj94@gmail.com>
2020-11-19 15:03:32 -0500

    signup boilerplate

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,3 +2,11 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { updateSignup } from '../actions/signup.js'
 import { signup } from '../actions/currentUser.js';
+
+const Signup = ({}) => {
+    return (
+        <form></form>
+    )
+}
+
+export default Signup
