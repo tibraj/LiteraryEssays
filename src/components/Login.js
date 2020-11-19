65b2bf740b247dc0120dd81bef7a34bff510bc91
tibraj <tibraj94@gmail.com>
2020-11-19 15:38:20 -0500

    handling onChange event

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,6 +4,14 @@ import { updateLogin } from '../actions/login.js'
 import { login } from '../actions/currentUser.js';
 
 const Login = ({}) => {
+    const handleOnChange = event => {
+        const {name, value} = event.target
+        const updatedInfo = {
+            ...loginData,
+            [name]: value 
+        }
+        updateLogin(updatedInfo)
+    }
     return (
         <form>
         </form>
