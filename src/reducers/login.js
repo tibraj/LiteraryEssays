adfba75691a064a2ed897dfe3ecd0eead4a2092b
tibraj <tibraj94@gmail.com>
2020-11-19 15:27:11 -0500

    switch case to update login form

--- src/reducers/login.js
+++ src/reducers/login.js
@@ -2,3 +2,12 @@ const initialState = {
     username: "",
     password: ""
 }
+
+export default (state=initialState, action) => {
+    switch (action.type) {
+        case "UPDATE_LOGIN":
+            return action.data
+        default:
+            return state
+    }
+}
