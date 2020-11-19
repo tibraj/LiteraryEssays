74a4b851fa36d5a7ec6aa9c4e741a6bd862456f9
tibraj <tibraj94@gmail.com>
2020-11-19 15:37:35 -0500

    login form component boilerplate

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,3 +2,12 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { updateLogin } from '../actions/login.js'
 import { login } from '../actions/currentUser.js';
+
+const Login = ({}) => {
+    return (
+        <form>
+        </form>
+    )
+}
+
+export default Login
