cd49bc306f31f797a7cb57a9fbb19a5225461b5f
tibraj <tibraj94@gmail.com>
2020-11-20 12:25:16 -0500

    switch case to set essays

--- src/reducers/essays.js
+++ src/reducers/essays.js
@@ -1,1 +1,10 @@
 const initialState = []
+
+export default (state=initialState, action) => {
+    switch (action.type) {
+        case "SET_ESSAYS":
+            return action.essays 
+        default:
+            return state
+    }
+}
