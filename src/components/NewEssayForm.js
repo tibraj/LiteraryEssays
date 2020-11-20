d7c82fd2a5a03905781b0c60cd255ef428d06a5c
tibraj <tibraj94@gmail.com>
2020-11-20 13:04:03 -0500

    import create essay action

--- src/components/NewEssayForm.js
+++ src/components/NewEssayForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {updateEssayForm} from '../actions/newEssayForm.js';
+import {createEssay} from '../actions/essays.js'
 
 const EssayForm = ({}) => {
     return (
