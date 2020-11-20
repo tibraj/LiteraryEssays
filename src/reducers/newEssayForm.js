19d887fd1838d36dd3621c6b441146e6c8756225
tibraj <tibraj94@gmail.com>
2020-11-20 11:45:54 -0500

    switch case for updating new essay form

--- src/reducers/newEssayForm.js
+++ src/reducers/newEssayForm.js
@@ -2,3 +2,16 @@ const initialState = {
     title: "",
     content: ""
 }
+
+export default (state=initialState, action) => {
+    switch (action.type) {
+        case "UPDATE_ESSAY_FORM":
+            const values = {
+                ...state,
+                [action.data.name]: action.data.value
+            }
+            return values
+        default:
+            return state
+    }
+}
