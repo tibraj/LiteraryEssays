e962e904d2d522eb045776b0996578205bd4cce5
tibraj <tibraj94@gmail.com>
2020-11-20 13:03:00 -0500

    essay form component boilerplate

--- src/components/NewEssayForm.js
+++ src/components/NewEssayForm.js
@@ -1,3 +1,11 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {updateEssayForm} from '../actions/newEssayForm.js';
+
+const EssayForm = ({}) => {
+    return (
+        <form></form>
+    )
+}
+
+export default EssayForm
