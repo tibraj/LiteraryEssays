1c78970d4b23b483dfd8ba606b43abd96eebe92f
tibraj <tibraj94@gmail.com>
2020-11-20 12:27:13 -0500

    switch case to add essay

--- src/reducers/essays.js
+++ src/reducers/essays.js
@@ -4,6 +4,8 @@ export default (state=initialState, action) => {
     switch (action.type) {
         case "SET_ESSAYS":
             return action.essays 
+        case "ADD_ESSAY":
+            return state.concat(action.essay)
         default:
             return state
     }
