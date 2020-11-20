5b607a5b6bdbca2f54272c1227143d010a78cfe2
tibraj <tibraj94@gmail.com>
2020-11-20 13:09:10 -0500

    prevent event default upon submission

--- src/components/NewEssayForm.js
+++ src/components/NewEssayForm.js
@@ -3,12 +3,13 @@ import {connect} from 'react-redux';
 import {updateEssayForm} from '../actions/newEssayForm.js';
 import {createEssay} from '../actions/essays.js'
 
-const EssayForm = ({data, updateEssayForm, createEssay, userId, essay}) => {
+const EssayForm = ({data, updateEssayForm, createEssay, userId, essay, history}) => {
     const handleChange = event => {
         const {name, value} = event.target
         updateEssayForm(name, value)
     }
-    const handleSubmit = (data, userId) => {
+    const handleSubmit = (event) => {
+        event.preventDefault()
         createEssay({...data, userId}, history)
     }
     return (
