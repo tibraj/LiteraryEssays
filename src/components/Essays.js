8427242e7808359e6125f1daabcec76160f6d6b8
tibraj <tibraj94@gmail.com>
2020-11-21 15:45:02 -0500

    essays component boilerplate

--- src/components/Essays.js
+++ src/components/Essays.js
@@ -1,3 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+
+const Essays = ({}) => {
+    return (
+        userEssays
+    )
+}
+
+export default Essays
