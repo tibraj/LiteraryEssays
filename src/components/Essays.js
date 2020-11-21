00aa447190dd9d4caefb52fe2f23dd35394d101a
tibraj <tibraj94@gmail.com>
2020-11-21 15:50:10 -0500

    essays component lists a user's essays by title and content

--- src/components/Essays.js
+++ src/components/Essays.js
@@ -2,7 +2,15 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-const Essays = ({}) => {
+const Essays = ({Essays}) => {
+    const userEssays = Essays.length > 0 ?
+    Essays.map (essay => (
+        <div>
+            <Link to={`/essays/${essay.id}`}><h1>{essay.title}</h1></Link>
+            <br></br>
+            <p>{essay.content}</p>
+        </div> 
+    )) : null
     return (
         userEssays
     )
