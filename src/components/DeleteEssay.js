29b5a9cdcd55c955f3f1d9ca162e04835021e643
tibraj <tibraj94@gmail.com>
2020-11-22 13:19:08 -0500

    delete essay boilerplate

--- src/components/DeleteEssay.js
+++ src/components/DeleteEssay.js
@@ -2,3 +2,14 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { removeEssay } from '../actions/essay.js';
+
+const DeleteEssay = ({}) => {
+    
+    return (
+        <form>
+
+        </form>
+    )
+}
+
+export default DeleteEssay
